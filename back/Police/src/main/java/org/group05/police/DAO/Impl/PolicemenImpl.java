a8f9f3612f5264a2cde693e9146385bcde439e37
Aurora-QY <129862200+Aurora-QY@users.noreply.github.com>
2023-12-26 23:04:06 +0800

    register部分更新

--- back/Police/src/main/java/org/group05/police/DAO/Impl/PolicemenImpl.java
+++ back/Police/src/main/java/org/group05/police/DAO/Impl/PolicemenImpl.java
@@ -21,4 +21,36 @@ public class PolicemenImpl implements PolicemenMapper{
         return !police.isEmpty();
     }
 
+    public boolean register(Map<String, String> regRequest, SqlSession sqlSession) {
+        try {
+            sqlSession.insert("PolicemenMapper.registerPolice", regRequest);
+            sqlSession.commit();
+            return true;
+        } catch (Exception e) {
+            sqlSession.rollback();
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean validate(Map<String, String> validationRequest, SqlSession sqlSession) {
+        // 根据 validationRequest 中的 queryType 决定使用哪个 SQL 映射。
+        String queryType = validationRequest.get("queryType");
+        String queryContent = validationRequest.get("queryContent");
+        String mapperId = "";
+        if ("police_number".equals(queryType)) {
+            mapperId = "PolicemenMapper.validatePoliceNumber";
+        } else if ("ID_number".equals(queryType)) {
+            mapperId = "PolicemenMapper.validateIDNumber";
+        }
+
+        try {
+            int count = sqlSession.selectOne(mapperId, queryContent);
+            return count == 0; // 如果没有记录，返回 true 表示校验通过
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
